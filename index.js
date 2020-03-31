7a73021fb0e74e98f3f8fee5ef122424e02d5381
Kfir Nevo <nevo.kfir@gmail.com>
2020-03-31 17:20:39 +0300

    fixed imports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
-import Item from './Item';
+import Item from './components/Item';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
